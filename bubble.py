bebc36109f655f7c6e21a8888b4949e070260bcc
lucie-cloud <luhaya@yahoo.com.br>
2023-12-19 20:02:52 -0300

    Commit

---
+++ bubble.py
@@ -0,0 +1,20 @@
+# Ordenação Bubble Sorting
+
+# Apresentação
+print('\n\t\t\t Bubble Sorting -- \n')
+
+n = []
+
+# Entrada
+quantidade = int(input('Quantidade de números para ordenar'))
+
+for i in range(0, quantidade):
+    n.append(int(input('Informe o {}º valor: '.format(i+1))))
+
+# Processamento
+for i in range(0, len(n)-1):
+    for j in range((i+1)), quantidade
+        if
+            n[i] > n[j]:
+            swap = n[j]
+            n[i] = swap
